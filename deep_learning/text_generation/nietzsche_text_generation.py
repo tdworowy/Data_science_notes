c01d46d62b216c0c2b652b5fad4ef6d7c61c7bc4
Tomasz <dworowytomasz@gmail.com>
2020-04-12 22:48:11 +0200

    update

--- deep_learning/text_generation/nietzsche_text_generation.py
+++ deep_learning/text_generation/nietzsche_text_generation.py
@@ -40,7 +40,7 @@ model.add(layers.LSTM(128, input_shape=(maxlen, len(chars))))
 model.add(layers.Dense(len(chars), activation='softmax'))
 
 optimizer = optimizers.RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=["acc"])
 
 
 def sample(preds, temperature=1.0):
@@ -55,16 +55,17 @@ def sample(preds, temperature=1.0):
 max_epoch = 20
 model.fit(x, y, batch_size=128, epochs=max_epoch)
 model.save('nietzsche.h5')
-#model = load_model('nietzsche.h5')
+# model = load_model('nietzsche.h5')
 
-start_index = random.randint(0, len(text) - maxlen - 1)
-generated_text = text[start_index: start_index + maxlen]
-for temperature in [0.6]:  # [0.2, 0.5, 1.0, 1.2]:
-    print('------Temperature:', temperature)
-    for i in range(400):
-        sampled = np.zeros((1, maxlen, len(chars)))
-        for t, char in enumerate(generated_text):
-            sampled[0, t, char_indices[char]] = 1.
+for i in range(10):
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    generated_text = text[start_index: start_index + maxlen]
+    for temperature in [0.6]:  # [0.2, 0.5, 1.0, 1.2]:
+        print('------Temperature:', temperature)
+        for i in range(400):
+            sampled = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(generated_text):
+                sampled[0, t, char_indices[char]] = 1.
             preds = model.predict(sampled, verbose=0)[0]
             next_index = sample(preds, temperature)
             next_char = chars[next_index]
