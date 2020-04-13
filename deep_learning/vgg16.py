9695dd3a2776672e81b59f5cd920d9a6b5d8d122
Tomasz <dworowytomasz@gmail.com>
2020-04-13 11:34:09 +0200

    update

--- deep_learning/vgg16.py
+++ deep_learning/vgg16.py
@@ -1,5 +1,7 @@
-from keras.applications import VGG16
+import ssl
 
+from keras.applications import VGG16
+ssl._create_default_https_context = ssl._create_unverified_context
 conv_base = VGG16(weights='imagenet',
                   include_top=False,
                   input_shape=(150, 150, 3))
