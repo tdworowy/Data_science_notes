c8188ec2fb22f32da2d42b6eaa5389ef1fc9addb
Tomasz <dworowytomasz@gmail.com>
2020-06-21 09:05:23 +0200

    update

---
+++ reinforcement_learning/gym/multi_arm_bandit_problem_softmax.py
@@ -0,0 +1,36 @@
+import random
+import math
+import gym
+import gym_bandits
+import numpy as np
+
+
+def softmax(tau, q):
+    total = sum([math.exp(val / tau) for val in q])
+    probs = [math.exp(val / tau) / total for val in q]
+    threshold = random.random()
+
+    cumulative_probe = 0.0
+    for i, probe in enumerate(probs):
+        cumulative_probe += probe
+        if cumulative_probe > threshold:
+            return i
+    return np.argmax(probs)
+
+
+if __name__ == "__main__":
+    num_rounds = 20000
+    count = np.zeros(10)
+    sum_rewards = np.zeros(10)
+    q = np.zeros(10)
+
+    env = gym.make('BanditTenArmedGaussian-v0')
+
+    for i in range(num_rounds):
+        arm = softmax(tau=0.5, q=q)
+        observation, reward, done, info = env.step(arm)
+        count[arm] += 1
+        sum_rewards[arm] += reward
+
+        q[arm] = sum_rewards[arm]/count[arm]
+    print(f"Optimal arm is {np.argmax(q)}")
