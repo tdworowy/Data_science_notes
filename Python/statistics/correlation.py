2bbe4fe7e0da7de8890c384624314c68ab1c698a
Tomasz <dworowytomasz@gmail.com>
2020-07-08 11:05:39 +0200

    update

---
+++ Python/statistics/correlation.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from scipy.stats import pearsonr
+
+if __name__ == "__main__":
+    boston = datasets.load_boston()
+    boston = pd.DataFrame(data=boston.data, columns=boston.feature_names)
+
+    print(boston)
+    correlation, _ = pearsonr(boston["TAX"], boston["B"])
+    print(f"Correlation between TAX and B: {correlation}")
+
+    # scatter plot
+    boston.plot.scatter(x="TAX", y="B") # x and y are columns names from boston data frame
+    plt.show()
--- R/Scripts/statistics/correlation.R
+++ R/Scripts/statistics/correlation.R
@@ -7,6 +7,8 @@ sp500 <- sp500[ep[2:(length(ep)-1)]]
 sp500$sp500_ret <- log(sp500$Close) - lag(log(sp500$Close))
 sp500 <- na.exclude(sp500)
 
+#corrplot
+#example1
 library(corrplot)
 corrplot(cor(sp500),method = "ellipse")
 corrplot(cor(sp500),method = "color")
@@ -21,3 +23,13 @@ corrplot(cor(catsM[c("Bwt","Hwt")]))
 #example3
 data("melanoma")
 corrplot(cor(melanoma))
+
+#scatter plot
+#example1
+library('MASS')
+data('mammals')
+mammals_small <- subset(mammals, mammals$body < 3)
+plot(mammals_small$body,mammals_small$brain)
+
+mammals_big <- subset(mammals, mammals$body > 100 & mammals$body <1000)
+plot(mammals_big$body,mammals_big$brain)
