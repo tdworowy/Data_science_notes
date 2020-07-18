0053eab343a1110eb0855e14b333e0d9f39953b3
Tomasz <dworowytomasz@gmail.com>
2020-07-18 10:07:47 +0200

    update

--- R/Scripts/statistics/data_distribution_and_sampling/bootstrap.R
+++ R/Scripts/statistics/data_distribution_and_sampling/bootstrap.R
@@ -1,5 +1,13 @@
 library(boot)
 data <- read.csv("Data/loans_income.csv")
 
-stat_fun <- function(x, idx) median(x[idx])
+stat_fun <- function(x, idx) median(x[idx,])
 boot_object <- boot(data, R=1000, statistic = stat_fun)
+
+# density plot original
+hist(data[["x"]], freq = FALSE)
+lines(density(data[["x"]]), lwd=3, col="green") 
+
+# density plot bootstrapped
+hist(boot_object[["t"]], freq = FALSE)
+lines(density(boot_object[["t"]]), lwd=3, col="green") 
