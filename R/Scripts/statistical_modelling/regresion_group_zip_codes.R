11ff433588b2c067362a1d16a24f61a23ffd872b
Tomasz <dworowytomasz@gmail.com>
2020-08-22 18:05:16 +0200

    update

---
+++ R/Scripts/statistical_modelling/regresion_group_zip_codes.R
@@ -0,0 +1,24 @@
+library(dplyr)
+data <- read.csv("Data/house_sales.csv", sep='\t')
+
+
+house_regresion <- lm(AdjSalePrice ~ SqFtTotLiving + SqFtLot + Bathrooms + 
+                       Bedrooms + BldgGrade,
+                     data=data, na.action=na.omit)
+
+zip_groups <- data %>%
+  mutate(resid=residuals(house_regresion)) %>%
+  group_by(ZipCode) %>%
+  summarise(med_resid = median (resid),
+            cnt = n()) %>%
+  arrange(med_resid) %>%
+  mutate(cum_cnt = cumsum(cnt),
+         ZipGroup = ntile(cum_cnt,5))
+
+house <- data %>% left_join(select(zip_groups, ZipCode, ZipGroup), by = "ZipCode")
+
+house_regresion
+
+house_regresion2 <- lm(AdjSalePrice ~ SqFtTotLiving + SqFtLot + Bathrooms + 
+                        Bedrooms + BldgGrade + ZipGroup,
+                      data=house, na.action=na.omit)
