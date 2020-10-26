bf84ed98664592f51129ebbf38a2c6dcf4d2a5f7
Tomasz <dworowytomasz@gmail.com>
2020-10-26 09:59:10 +0100

    up

--- R/Scripts/statistical_machine_learning/caret/example.R
+++ R/Scripts/statistical_machine_learning/caret/example.R
@@ -35,3 +35,36 @@ testing = segmentationOriginal[-in_train,]
 fit <- train(Case ~ ., data = training, nethod = 'rpart')
 
 fit$finalModel
+
+
+#___________
+
+library(caret)
+library(gbm)
+set.seed(3433)
+library(AppliedPredictiveModeling)
+
+data(AlzheimerDisease)
+
+adData = data.frame(diagnosis,predictors)
+
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+set.seed(62433)
+
+fit1 <- train(diagnosis ~ ., data = training, nethod = 'rf')
+fit2 <- train(diagnosis ~ ., data = training, nethod = 'gbm')
+fit3 <- train(diagnosis ~ ., data = training, nethod = 'lda')
+
+predict_rf<-predict(fit1, newdata = testing)
+predict_gbm<-predict(fit2, newdata = testing)
+predict_lda<-predict(fit3, newdata = testing)
+
+predDF <- data.frame(predict_rf, predict_gbm, predict_lda, diagnosis = testing$diagnosis)
+
+combModFit <- train(diagnosis ~., method = "rf",data = predDF)
+combModFit$finalModel
+summary(combModFit)
