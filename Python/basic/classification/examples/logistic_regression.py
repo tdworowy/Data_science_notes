4d9555136259b909626c1f9514979cfba2a35fe8
Tomasz <dworowytomasz@gmail.com>
2020-12-18 13:56:07 +0100

    update

---
+++ Python/basic/classification/examples/logistic_regression.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+from Python.basic.classification.data_preparation import get_data
+from Python.basic.classification.utils import calculate_metrics
+
+if __name__ == '__main__':
+    x_train, x_test, y_train, y_test = get_data()
+
+    logistic_regression_classifier = LogisticRegression(verbose=3)
+    logistic_regression_classifier.fit(x_train, y_train)
+    y_pred = logistic_regression_classifier.predict(x_test)
+
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    accuracy, recall, precision = calculate_metrics(cm)
+    print(f'accuracy:{accuracy}, recall:{recall}, precision: {precision}')
---
+++ Python/basic/classification/examples/svm.py
@@ -0,0 +1,17 @@
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+
+from Python.basic.classification.data_preparation import get_data
+from Python.basic.classification.utils import calculate_metrics
+
+if __name__ == '__main__':
+    x_train, x_test, y_train, y_test = get_data()
+
+    classifier = SVC(verbose=3, kernel='linear')
+    classifier.fit(x_train, y_train)
+    y_pred = classifier.predict(x_test)
+
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    accuracy, recall, precision = calculate_metrics(cm)
+    print(f'accuracy:{accuracy}, recall:{recall}, precision: {precision}')
